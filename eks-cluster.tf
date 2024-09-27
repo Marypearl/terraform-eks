9f6fcc4f9dc0ba9adaca5f92e6db26cf1eed9e43
Mary O Erhabor <55197252+Marypearl@users.noreply.github.com>
2024-09-26 22:48:29 -0400

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -5,8 +5,8 @@
 #  * EKS Cluster
 #
 
-resource "aws_iam_role" "tsp-cluster" {
-  name = "tsp-cluster"
+resource "aws_iam_role" "project-cluster" {
+  name = "project-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -24,20 +24,20 @@ resource "aws_iam_role" "tsp-cluster" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "tsp-cluster-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "project-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.tsp-cluster.name
+  role       = aws_iam_role.project-cluster.name
 }
 
-resource "aws_iam_role_policy_attachment" "tsp-cluster-AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "project-cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.tsp-cluster.name
+  role       = aws_iam_role.project-cluster.name
 }
 
-resource "aws_security_group" "tsp-cluster" {
-  name        = "tsp-cluster-sg"
+resource "aws_security_group" "project-cluster" {
+  name        = "project-cluster-sg"
   description = "Cluster communication with worker nodes"
-  vpc_id      = aws_vpc.tsp-vpc.id
+  vpc_id      = aws_vpc.project-vpc.id
 
   egress {
     from_port   = 0
@@ -47,31 +47,31 @@ resource "aws_security_group" "tsp-cluster" {
   }
 
   tags = {
-    Name = "tsp-cluster"
+    Name = "project-cluster"
   }
 }
 
-resource "aws_security_group_rule" "tsp-cluster-ingress-workstation-https" {
+resource "aws_security_group_rule" "project-cluster-ingress-workstation-https" {
   cidr_blocks       = [local.workstation-external-cidr]
   description       = "Allow workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = aws_security_group.tsp-cluster.id
+  security_group_id = aws_security_group.project-cluster.id
   to_port           = 443
   type              = "ingress"
 }
 
-resource "aws_eks_cluster" "tsp-cluster" {
+resource "aws_eks_cluster" "project-cluster" {
   name     = var.cluster-name
-  role_arn = aws_iam_role.tsp-cluster.arn
+  role_arn = aws_iam_role.project-cluster.arn
 
   vpc_config {
-    security_group_ids = [aws_security_group.tsp-cluster.id]
-    subnet_ids         = aws_subnet.tsp-vpc[*].id
+    security_group_ids = [aws_security_group.project-cluster.id]
+    subnet_ids         = aws_subnet.project-vpc[*].id
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.tsp-cluster-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.tsp-cluster-AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.project-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.project-cluster-AmazonEKSServicePolicy,
   ]
 }
