c3588b060320cd121f0abe9ec6c3605c654f72d2
Mary O Erhabor <55197252+Marypearl@users.noreply.github.com>
2024-09-26 22:55:03 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,49 +6,49 @@
 #  * Route Table
 #
 
-resource "aws_vpc" "tsp-vpc" {
+resource "aws_vpc" "project-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = tomap({
-    "Name" = "tsp-eks-node",
+    "Name" = "project-eks-node",
     "kubernetes.io/cluster/${var.cluster-name}" = "shared",
   })
 }
 
-resource "aws_subnet" "tsp-vpc" {
+resource "aws_subnet" "project-vpc" {
   count = 2
 
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
-  vpc_id                  = aws_vpc.tsp-vpc.id
+  vpc_id                  = aws_vpc.project-vpc.id
 
   tags = tomap({
-    "Name" = "tsp-eks-node",
+    "Name" = "project-eks-node",
     "kubernetes.io/cluster/${var.cluster-name}" = "shared",
   })
 }
 
-resource "aws_internet_gateway" "tsp-vpc" {
-  vpc_id = aws_vpc.tsp-vpc.id
+resource "aws_internet_gateway" "project-vpc" {
+  vpc_id = aws_vpc.project-vpc.id
 
   tags = {
-    Name = "tsp-vpc"
+    Name = "project-vpc"
   }
 }
 
-resource "aws_route_table" "tsp-vpc" {
-  vpc_id = aws_vpc.tsp-vpc.id
+resource "aws_route_table" "project-vpc" {
+  vpc_id = aws_vpc.project-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.tsp-vpc.id
+    gateway_id = aws_internet_gateway.project-vpc.id
   }
 }
 
-resource "aws_route_table_association" "tsp-vpc" {
+resource "aws_route_table_association" "project-vpc" {
   count = 2
 
-  subnet_id      = aws_subnet.tsp-vpc.*.id[count.index]
-  route_table_id = aws_route_table.tsp-vpc.id
+  subnet_id      = aws_subnet.project-vpc.*.id[count.index]
+  route_table_id = aws_route_table.project-vpc.id
 }
